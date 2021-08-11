05b4b28afc32d886b2baf918286c764382a68fb1
Benson <bensongathu23@gmail.com>
2021-08-11 19:22:38 +0300

    load static files fixed

--- app/views.py
+++ app/views.py
@@ -5,10 +5,8 @@ from .models import Project,Languages,Profile
 def index(request):
     projects = Project.objects.all()
     all_languages = Languages.objects.all()
-    current_user = request.user.profile
     context = {
         "projects":projects,
         "all_languages":all_languages,
-        "current_user":current_user,
     }
     return render(request,"index.html",context)
